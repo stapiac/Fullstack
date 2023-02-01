9c1495d7c30e1407680d51e15c6bd355d95d2db8
Sergio Tapia <stapias@uninorte.edu.co>
2023-02-01 09:13:24 -0500

    Second commit

---
+++ db/dbConnect.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+async function dbConnect() {
+  mongoose
+    .connect(process.env.DB_URL, {
+      //   these are options to ensure that the connection is done properly
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      //useCreateIndex: true, no longer supported, it has been deprecated
+    })
+    .then(() => {
+      console.log("Successfully connected to MongoDB Atlas!");
+    })
+    .catch((error) => {
+      console.log("Unable to connect to MongoDB Atlas!");
+      console.error(error);
+    });
+}
+
+module.exports = dbConnect;
---
+++ db/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+});
+
+module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
+
+//The code above is saying:
+//"create a user table or collection if there is no table with that name already".
